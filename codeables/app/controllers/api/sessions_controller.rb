f3ce36995e41a2f34e6b67147c4f2696d59e42f1
Yohanan Radchenko <yohananradchenko@gmail.com>
2017-02-14 15:00:08 -0500

    ajax request to server to create and login user

--- codeables/app/controllers/api/sessions_controller.rb
+++ codeables/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionsController < ApplicationController
+  def create
+    username = params[:user][:username]
+    password = params[:user][:password]
+    @user = User.find_by_credentials(username, password)
+
+    if @user
+      login_user(@user)
+      render 'api/users/show'
+    else
+      render json: ['Invalid username/password combination'], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render json: ['Session controller needs redirect logic']
+    else
+      render json: ['Nobody signed in'], status: 404
+    end
+  end
 end
