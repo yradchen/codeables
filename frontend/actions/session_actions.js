0bd4823cf30d1a67639b8f7caf9cd1e91edb8352
Yohanan Radchenko <yohananradchenko@gmail.com>
2017-02-15 18:53:41 -0500

    remove nested folder

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,32 @@
+import * as SessionAPIUtil from '../util/SESSion_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const login = (user) => dispatch => (
+  SessionAPIUtil.login(user)
+    .then(currentUser => dispatch(receiveCurrentUser(currentUser)),
+      errors => dispatch(receiveErrors(errors.responseJSON))
+    )
+);
+
+export const signup = (user) => dispatch => (
+  SessionAPIUtil.signup(user)
+    .then(currentUser => dispatch(receiveCurrentUser(currentUser)),
+      errors => dispatch(receiveErrors(errors.responseJSON))
+    )
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout().then(user => dispatch(receiveCurrentUser(null)))
+);
