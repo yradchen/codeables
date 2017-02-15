eabc7335785fba2b13a5975dfdb9dbdc867acc70
= <=>
2017-02-15 11:24:55 -0500

    add functional login/logout buttons"

--- codeables/frontend/components/user/user_form.jsx
+++ codeables/frontend/components/user/user_form.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const UserForm = ({ currentUser, logout }) => {
-    return <h1>This will one day be the header</h1>
+const sessionLinks = () => {
+  return (
+    <nav className='login-signup'>
+      <Link to="/account/login">Login</Link>
+      <Link to="/account/register">Sign up</Link>
+    </nav>
+)};
 
+const loggedin = (currentUser, logout) => {
+  return (
+    <nav>
+      <p>{currentUser.username}</p>
+      <button onClick={logout}>Log Out</button>
+    </nav>
+  )
+}
+
+const UserForm = ({ currentUser, logout }) => {
+  debugger
+  return currentUser ? loggedin(currentUser, logout) : sessionLinks()
 };
 
 export default UserForm;
 
-// A <Link to> /#/signup
-// A <Link to> /#/login
+// const personalGreeting = (currentUser, logout) => (
+// 	<hgroup className="header-group">
+//     <h2 className="header-name">Hi, {currentUser.username}!</h2>
+//     <button className="header-button" onClick={logout}>Log Out</button>
+// 	</hgroup>
+// );
