de91c55c134a8dd44616af7f1a8e73e9c0c734f0
Yohanan Radchenko <yohananradchenko@gmail.com>
2017-02-15 11:26:19 -0500

    add header and footer change routes to display header and footer properly

---
+++ codeables/frontend/components/nav_bar/nav_bar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserFormContainer from './user_form_container';
+import SessionFormContainer from '../session_form/session_form_container';
+
+const NavBar = () => {
+  return (
+    <header>
+    <p>other nav bar stuff</p>
+    <UserFormContainer />
+  </header>
+  );
+};
+
+export default NavBar;
