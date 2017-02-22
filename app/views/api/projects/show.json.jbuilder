e2ab1e6f77f648e13ae04b4201921b5bbbd05b36
Yohanan Radchenko <yohananradchenko@gmail.com>
2017-02-22 17:40:30 -0500

    add show page for projects

--- app/views/api/projects/show.json.jbuilder
+++ app/views/api/projects/show.json.jbuilder
@@ -1,8 +1,10 @@
 json.extract! @project, :title, :description, :cover_img, :id
+json.owner @project.user.username
 
 json.instructions do
   json.array! @project.instructions do |instruction|
     json.extract! instruction, :id, :step_title, :step_detail, :media, :project_id
+
     json.media asset_path(instruction.media.url)
   end
 end
--- frontend/components/projects/project_detail.jsx
+++ frontend/components/projects/project_detail.jsx
@@ -8,9 +8,41 @@ class ProjectDetail extends React.Component {
   }
   render () {
     if (this.props.project === undefined) return null;
-    debugger
+
+    const instructions = this.props.project.instructions.map( (instruction) => {
+      return (
+      <div>
+        <h2 className="show-instruction-title">{instruction.step_title}</h2>
+        <div className="show-img-container">
+          <img className="show-img" src={instruction.media}/>
+        </div>
+        <pre className="show-description">{instruction.step_detail}</pre>
+      </div>
+      );
+    });
+
     return (
-      <p>hi!</p>
+      <div className="show-outer">
+        <div className="show-inner">
+          <header className="show-top">
+            <div className="owner-info">
+              <h1>{this.props.project.title}</h1>
+              <span className="all">
+              <p className="by">by</p>
+              <p className="show-owner">{this.props.project.owner}</p>
+              </span>
+            </div>
+            <img src={images.logo} alt="logo" className="show-logo"/>
+          </header>
+          <section className="show-bottom">
+            <div className="show-img-container">
+              <img className="show-img" src={this.props.project.cover_img}/>
+            </div>
+            <pre className="show-description">{this.props.project.description}</pre>
+            {instructions}
+          </section>
+        </div>
+      </div>
     );
 
   }
